f88db5732fcca9dece7c0e460aa7ca103447266b
Drygan_Dev <gigo12121212@gmail.com>
2023-11-26 11:48:04 +0100

    Profile updated, remove unused code

--- Urbatone/ViewControllers/ProfileViewController.swift
+++ Urbatone/ViewControllers/ProfileViewController.swift
@@ -11,10 +11,15 @@ class ProfileViewController: BaseViewController {
     
     @IBOutlet weak var singOutButton: UIButton!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         singOutButton.addTarget(self, action: #selector(signOut), for: .touchUpInside)
+        imageView.layer.cornerRadius = 60
     }
     
     @objc func signOut() {
